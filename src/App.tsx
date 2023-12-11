c98f16b4c3f4814836356051faf3eae7f4255e91
Kelvin Charles <kelvinc1291@hotmail.com>
2023-12-11 20:00:14 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { styled } from "styled-components";
+import { EstilosGlobais } from "./components/EstilosGlobais";
+
+const FundoGradiente = styled.div`
+  background: var(
+    --Gradiente-fundo,
+    linear-gradient(175deg, #041833 4.16%, #04244f 48%, #154580 96.76%)
+  );
+  width: 100%;
+  height: 100vh;
+`;
+
+function App() {
+  return (
+  <FundoGradiente>
+    <EstilosGlobais/>
+  </FundoGradiente>
+  )
+}
+
+export default App;
