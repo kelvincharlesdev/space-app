a8ec14116c885f639722f1f9b6081c66cf413d92
Kelvin Charles <kelvinc1291@hotmail.com>
2023-12-18 20:13:54 -0300

    fix: Corrige importação da logo

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -7,6 +7,7 @@ const HeaderEstilizado = styled.header`
   padding: 60px 0;
   display: flex;
   justify-content: space-between;
+  align-items: center;
   img {
     max-width: 212px;
   }
@@ -17,7 +18,7 @@ const HeaderEstilizado = styled.header`
 export const Cabecalho = () => {
   return (
     <HeaderEstilizado>
-      <img src="public/imagens/logo.png" alt="Imagem da Logo" />
+      <img src="/imagens/logo.png" alt="Imagem da Logo" />
 
      <CampoTexto img={search}/>
     </HeaderEstilizado>
