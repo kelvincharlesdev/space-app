c7dd0e79f2d00032ed5c0548a7c5cf38393ddc20
Kelvin Charles <kelvinc1291@hotmail.com>
2023-12-19 20:20:09 -0300

    fix: Adiciona mais itens de navegação

--- src/components/BarraLateral/index.tsx
+++ src/components/BarraLateral/index.tsx
@@ -28,6 +28,30 @@ export const BarraLateral = () => {
                 >
                     Mais vistas
                 </ItemNavegacao>
+
+                <ItemNavegacao
+                    iconeAtivo="/icones/mais-curtidas-ativo.png"
+                    iconeInativo="/icones/mais-curtidas-inativo.png"
+                    ativo={false}
+                >
+                    Mais curtidas
+                </ItemNavegacao>
+
+                <ItemNavegacao
+                    iconeAtivo="/icones/novas-ativo.png"
+                    iconeInativo="/icones/novas-inativo.png"
+                    ativo={false}
+                >
+                   Novas
+                </ItemNavegacao>
+
+                <ItemNavegacao
+                    iconeAtivo="/icones/surpreenda-me-ativo.png"
+                    iconeInativo="/icones/surpreenda-me-inativo.png"
+                    ativo={false}
+                >
+                   Surpreenda-me
+                </ItemNavegacao>
             </ListaEstilizada>
         </aside>
     )
